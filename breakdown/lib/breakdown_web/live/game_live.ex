8181342564d8400d250c128ce862f8eccc5eccfa
MMcClure11 <mmcclure0100@gmail.com>
2023-09-05 10:18:36 -0400

    Apply formatting

--- breakdown/lib/breakdown_web/live/game_live.ex
+++ breakdown/lib/breakdown_web/live/game_live.ex
@@ -1,11 +1,11 @@
-defmodule BreakdownWeb.WordxLive do 
+defmodule BreakdownWeb.WordxLive do
   use BreakdownWeb, :live_view
 
-  def mount(_params, _session, socket) do 
+  def mount(_params, _session, socket) do
     {:ok, socket}
   end
 
-  def render(assigns) do 
+  def render(assigns) do
     ~H"""
     <pre>
       <%= inspect assigns, pretty: true %>
@@ -15,32 +15,32 @@ defmodule BreakdownWeb.WordxLive do
     """
   end
 
-  def everything(assigns) do 
+  def everything(assigns) do
     ~H"""
     <div class="mx-auto max-w-sm">
-    <header class="flex items-center justify-between gap-6"}>
-    <div>
-        <h1 class="text-lg font-semibold leading-8 text-zinc-800">
-        Hello,
-        </h1>
-        <p class="mt-2 text-sm leading-6 text-zinc-600">
-        World
-        </p>
-    </div>
-    </header>
+      <header class="flex items-center justify-between gap-6" }>
+        <div>
+          <h1 class="text-lg font-semibold leading-8 text-zinc-800">
+            Hello,
+          </h1>
+          <p class="mt-2 text-sm leading-6 text-zinc-600">
+            World
+          </p>
+        </div>
+      </header>
 
-    <br/>
-    <br/>
-    <hr/>
-    <br/>
-    <br/>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
 
-    <div class="grid grid-cols-5 gap-4 text-center font-bold">
+      <div class="grid grid-cols-5 gap-4 text-center font-bold">
         <div class="rounded border-2 border-gray-500 pt-2 pb-2 text-white">-</div>
-        <div class="rounded border-2 border-gray-500 pt-2 pb-2"> </div>
-        <div class="rounded border-2 border-gray-500 pt-2 pb-2"> </div>
-        <div class="rounded border-2 border-gray-500 pt-2 pb-2"> </div>
-        <div class="rounded border-2 border-gray-500 pt-2 pb-2"> </div>
+        <div class="rounded border-2 border-gray-500 pt-2 pb-2"></div>
+        <div class="rounded border-2 border-gray-500 pt-2 pb-2"></div>
+        <div class="rounded border-2 border-gray-500 pt-2 pb-2"></div>
+        <div class="rounded border-2 border-gray-500 pt-2 pb-2"></div>
 
         <div class="rounded bg-green-600 pt-2 pb-2 text-white">G</div>
         <div class="rounded bg-yellow-500 pt-2 pb-2 text-white">U</div>
@@ -71,15 +71,15 @@ defmodule BreakdownWeb.WordxLive do
         <div class="rounded bg-gray-500 pt-2 pb-2 text-white">E</div>
         <div class="rounded bg-green-600 pt-2 pb-2 text-white">S</div>
         <div class="rounded bg-yellow-500 pt-2 pb-2 text-white">S</div>
-    </div>
+      </div>
 
-    <br/>
-    <br/>
-    <hr/>
-    <br/>
-    <br/>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
 
-    <div class="grid grid-cols-10 gap-3 text-center font-bold">
+      <div class="grid grid-cols-10 gap-3 text-center font-bold">
         <div class="rounded bg-green-600 pt-1 pb-1 text-white">Q</div>
         <div class="rounded bg-yellow-500 pt-1 pb-1 text-white">W</div>
         <div class="rounded bg-gray-500 pt-1 pb-1 text-white">E</div>
@@ -112,7 +112,7 @@ defmodule BreakdownWeb.WordxLive do
         <div class="rounded border-2 border-solid border-slate-600 pt-1 pb-1 text-black">M</div>
         <div></div>
         <div></div>
-    </div>
+      </div>
     </div>
     """
   end
